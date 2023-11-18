095f4db975c2965d899f907c591eb0e38b5419dc
Egxon <94643935+Egxon@users.noreply.github.com>
2023-11-18 02:26:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -268,9 +268,7 @@ def display_page(pathname):
             html.H3('Graphiques'),
             html.Div(className="bigG", children=[
                 html.Div(className="bigG-block", children=[
-                    html.Div(className="bigG-block-top", children=[figure=px.histogram(totalG, x='name', y='totalGold', histfunc='max',
-                                                                      color='totalGold',
-                                                                      title="Total gold of LNG vs T1 , series 1"))
+                    html.Div(className="bigG-block-top", children=[
 
                         dcc.Graph(id="totalGold", figure=px.histogram(totalG, x='name', y='totalGold', histfunc='max',
                                                                       color='totalGold',
